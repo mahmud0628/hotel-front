a043ca0549ad3ed3e01e97021d607345c777db90
mahmud0628 <mahmudabdullayev48@gmail.com>
2022-04-27 12:59:24 +0500

    Dialog added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatIconModule } from '@angular/material/icon';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BinoComponent } from './entity/bino/bino.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DeleteDialogComponent } from './shared/delete-dialog/delete-dialog.component';
+import { MatDialogModule } from '@angular/material/dialog';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BinoComponent,
+    DeleteDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatDialogModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/entity/bino/bino.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bino',
+  templateUrl: './bino.component.html',
+  styleUrls: ['./bino.component.css']
+})
+export class BinoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
